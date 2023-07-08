671a41ea01acc9ee60e389891351b00a18eeea72
CMBhatt123 <Chandra.755421@snsc.tu.edu.np>
2023-07-08 12:22:18 +0545

    added card

--- index.php
+++ index.php
@@ -61,10 +61,33 @@
           <a class="nav-link" href="#">Login</a>
         </li>
 </ul>
+</nav>
 <!-- third child -->
 <div class="bg-light">
-  <h3 class="text-center">Hidden store</h3>
+  <h3 class="text-center">Hidden Store</h3>
+  <p class="text-center">Communication is the heart of e-commerce & community</p>
 </div>
+<!-- fourth section -->
+<div class="row">
+  <div class="col md-10">
+    <!-- products -->
+    <div class="row">
+      <div class="col-md-4"><div class="card">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+      <div class="col-md-4"></div>
+      <div class="col-md-4"></div>
+    </div>
+    </div>
+<div class="col md-2">
+<!-- side nav -->
+
+  </div>
 
 <!-- last child -->
 <!-- <div class="bg-info p-3 text-center">
